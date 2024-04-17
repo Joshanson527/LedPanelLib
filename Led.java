e08c0cd9abb0336cf35b3575d3bbbac67023001e
Josh <Joshanson527@gmail.com>
2024-04-17 13:27:20 -0700

    Update Led.java
    Added rainbow effect method

--- Led.java
+++ Led.java
@@ -1,9 +1,10 @@
-int height = 8
-int width = 32
-int port = 0
-
-ledPanel = new AddressableLED(port); // Set LED port
-ledBuffer = new AddressableLEDBuffer(height*width); // Set LED buffer length
+int height = 8;
+int width = 32;
+int port = 0;
+int rainbowHue = 0;
+  
+public ledPanel = new AddressableLED(port); // Set LED port
+public ledBuffer = new AddressableLEDBuffer(height*width); // Set LED buffer length
 
 public static void init() {}
   ledPanel.setLength(ledBuffer.getLength()); // Set LED strip length
@@ -12,21 +13,19 @@ public static void init() {}
   ledPanel.start(); // Start outputting to LEDs
 }
 
-public static void setPlateRGB(int[] color = new int[3]) {
-  for(int i = 0, i < ledbuffer.getLength(), i++) {
-    ledBuffer.setRGB(i, color[1], color[2], color[3]); // Loop through and set every LED to the color in the buffer
-  }
-  ledPanel.setData(ledBuffer); // Set the panel to the buffer
+public static void setPlateRGB(int[] color) {
+    for(int i = 0, i < ledbuffer.getLength(), i++) {
+      ledBuffer.setRGB(i, color[1], color[2], color[3]); // Loop through and set every LED to the color in the buffer
+    }
 }
 
-public static void setPlateHSV(int[] color = new int[3]) {
+public static void setPlateHSV(int[] color) {
   for(int i = 0, i < ledbuffer.getLength(), i++) {
     ledBuffer.setHSV(i, color[1], color[2], color[3]); // Loop through and set every LED to the color in the buffer
   }
-  ledPanel.setData(ledBuffer); // Set the panel to the buffer
 }
 
-private static void drawLineRGB(int x0, int y0, int x1, int y1, int[] color = new int[3]) {
+private static void drawLineRGB(int x0, int y0, int x1, int y1, int[] color) {
   double dx = Math.abs(x1 - x0);
   int sx = x0 < x1 ? 1 : -1;
   double dy = 0 - Math.abs(y1 - y0);
@@ -57,7 +56,7 @@ private static void drawLineRGB(int x0, int y0, int x1, int y1, int[] color = ne
   }
 }
 
-private static void drawLineHSV(int x0, int y0, int x1, int y1, int[] color = new int[3]) {
+private static void drawLineHSV(int x0, int y0, int x1, int y1, int[] color) {
   double dx = Math.abs(x1 - x0);
   int sx = x0 < x1 ? 1 : -1;
   double dy = 0 - Math.abs(y1 - y0);
@@ -87,3 +86,42 @@ private static void drawLineHSV(int x0, int y0, int x1, int y1, int[] color = ne
     }
   }
 }
+
+//@Param full    whether or not it fills the whole plate
+//@Param speed   How fast the hue changes
+//@Param x0      Starting x offset
+//@Param y0      Top y offset
+//@Param x1      Final X offset
+//@Param y1      Bottom y offset
+private static void rainbow(boolean full, int speed, int x0, int y0, int x1, int y1) {
+  if (full) {
+    for(int i = 0, i < ledbuffer.getLength(), i++) {
+      ledBuffer.setHSV(i, whatever, 1, 1);
+    }
+  } else {
+    for (var i = x0; i < x1; i++) {
+      final var hue = (rainbowHue + (i * 180 / width)) % 180;
+      drawLineHSV(i, y0, i, height - y1, hue, 255, 128);
+    }
+  }
+  rainbowHue += speed;
+  rainbowHue %= 180;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
